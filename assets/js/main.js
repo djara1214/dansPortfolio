b8e032c269d67eafc1094e6311baf2e63c76e2aa
[Danny Jaramillo] <[djaramillo1214@icloud.com]>
2024-08-11 13:24:30 -0400

    updated and finished main parts of site

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,6 @@ const sr = ScrollReveal({
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .home__more-about-img, .section-title_sub, .skills__img',{delay: 400}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .home__more-about-img, .section-title_sub',{delay: 300}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img',{interval: 200}); 
